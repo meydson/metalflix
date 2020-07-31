3c626dc40f46733a3e73d7080743455945b7c40c
Meydson Baracho <meydsonbaracho@gmail.com>
2020-07-31 01:43:33 -0300

    final aula 4

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -22,7 +22,9 @@ function CadastroCategoria() {
         setValue(infosDoEvento.target.getAttribute('name'), infosDoEvento.target.value);
      }
     useEffect(() => {
-        const URL_TOP = 'http://localhost:8080/categorias';
+        const URL_TOP = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://meydsonmetalflix.herokuapp.com/categorias';
         fetch(URL_TOP) 
             .then(async (respostaDoServidor) => {
                 const resposta = await respostaDoServidor.json();
